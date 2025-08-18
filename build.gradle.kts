4eb77365f4e9944c83a4b7fc0096939ffbf7e7af
kigawa <mail@kigawa.net>
2025-08-18 22:47:55 +0900

    Configure CI/CD pipeline for Maven Central deployment and update plugin configurations for publishing and signing integrations. Refactor artifact naming and align dependencies across modules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
 group = "net.kigawa"
@@ -20,4 +21,15 @@ tasks.test {
 
 kotlin {
     jvmToolchain(8)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
+        }
+    }
 }
