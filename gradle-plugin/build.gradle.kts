0ba648fc19f668f53f29502062546afe1c9347a3
kigawa <mail@kigawa.net>
2025-08-25 02:19:40 +0900

    Refactor build scripts to move Maven publishing configurations and signing setup to `gradle-plugin` module

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm")
     `java-gradle-plugin`
+    application
+    signing
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "net.kigawa"
@@ -44,3 +49,62 @@ java {
 }
 
 
+
+
+publishing {
+    // Configure all publications
+    publications.withType<MavenPublication> {
+        pom {
+            artifactId = "net.kigawa.renlin-compiler.gradle.plugin"
+            name.set("net.kigawa.renlin-compiler.gradle.plugin")
+            description.set("Kotlin Compiler Plugin for automatic value injection with @AutoFill annotation")
+            url.set("https://github.com/Code-Sakura/renlin-compiler")
+            properties.set(
+                mapOf(
+                )
+            )
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("http://www.opensource.org/licenses/mit-license.php")
+                }
+            }
+            developers {
+                developer {
+                    id.set("net.kigawa")
+                    name.set("kigawa")
+                    email.set("contact@kigawa.net")
+                }
+                developer {
+                    id.set("io.github.seizavl")
+                    name.set("seizavl")
+                    email.set("")
+                }
+            }
+            scm {
+                connection.set("scm:git:https://github.com/Code-Sakura/renlin-compiler.git")
+                developerConnection.set("scm:git:https://github.com/Code-Sakura/renlin-compiler.git")
+                url.set("https://github.com/Code-Sakura/renlin-compiler")
+            }
+        }
+    }
+}
+
+signing {
+    if (project.hasProperty("mavenCentralUsername") ||
+        System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername") != null
+    ) {
+        useGpgCmd()
+        // It is not perfect (fails at some dependency assertions), better handled as
+        // `signAllPublications()` (as in vanniktech maven publish plugin) at build.gradle.kts.
+        //sign(publishing.publications)
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    if (project.hasProperty("mavenCentralUsername") ||
+        System.getenv("ORG_GRADLE_PROJECT_mavenCentralUsername") != null
+    )
+        signAllPublications()
+}
